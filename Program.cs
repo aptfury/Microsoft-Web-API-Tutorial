ce7d8d5eae100e6772f0ebed593f116a87323531
aptfury <blake.lemarr@outlook.com>
2023-12-12 04:18:10 -0500

    updated gitignore

--- Program.cs
+++ Program.cs
@@ -1,52 +1,27 @@
-using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
 
-/* var builder = WebApplication.CreateSlimBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options =>
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    c.SwaggerDoc("v1", new OpenApiInfo {  Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
 });
 
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
-
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
-
-app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
+    c.SwaggerEndpoint("/swagger/v1/swagger/json", "PizzaStore API V1");
+});
 
-} */
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors(options => { });
-
-var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
-app.UseCors("some unique string as a test string");
-
-/**
- * From Tutorial:
- * Middleware is usually code that intercepts the request and carries out checks like checking for authentication or ensuring the client is allowed
- * to perform this operation according to CORS. After the middleware is done executing, the actual request is carried out. Data is either read or
- * written to the store and a response is sent back to the calling client.
- */
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
